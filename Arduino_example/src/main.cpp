53634afd2c15c940dd7748eab4a2aae5e2208d7d
Takayuki Ono <takaaaaadayo@gmail.com>
2024-01-22 08:43:03 +0900

    add EEG-alcohol-analysis

--- Arduino_example/src/main.cpp
+++ Arduino_example/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
